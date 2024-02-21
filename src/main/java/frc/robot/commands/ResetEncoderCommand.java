51576b7d6886ea4a5d5a980dd540f91e7dd9a365
Thierry Lachance <101730754+Thierry-Lachance@users.noreply.github.com>
2024-02-21 12:49:25 -0500

    Update ResetEncoderCommand.java
    changed to instantCommand

--- src/main/java/frc/robot/commands/ResetEncoderCommand.java
+++ src/main/java/frc/robot/commands/ResetEncoderCommand.java
@@ -4,10 +4,10 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Drivetrain;
 
-public class ResetEncoderCommand extends Command {
+public class ResetEncoderCommand extends InstantCommand {
   private Drivetrain drivetrain;
 
   /** Creates a new ResetEncoderCommand. */
@@ -19,21 +19,6 @@ public class ResetEncoderCommand extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
+  public void initialize() {
     drivetrain.resetEncoder();
   }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-}
