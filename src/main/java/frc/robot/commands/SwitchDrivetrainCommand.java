90b2124c4d67f513798340f51dbf37553d605bb0
Thierry Lachance <lachancethierry6@gmail.com>
2024-02-29 13:04:52 -0500

    fixed bug

--- src/main/java/frc/robot/commands/SwitchDrivetrainCommand.java
+++ src/main/java/frc/robot/commands/SwitchDrivetrainCommand.java
@@ -5,7 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.Drivetrain.*;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Drivetrain.possibleDriveState;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
